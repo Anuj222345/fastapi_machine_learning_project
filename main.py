4a3610068f22e1b9894f720fe8d0501cd62e7494
Anuj <rocketanuj0107@gmail.com>
2026-09-24 00:12:16 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return {'message': 'Hello, World'}
+
+@app.get('/Welcome')
+def get_name(name: str):
+    return {'Welcome To Anuj Youtube Channel': f'{name}'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
